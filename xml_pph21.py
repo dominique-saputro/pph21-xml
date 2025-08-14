c624d027312cc467d0fca04cea3a93fb1d5d2054
Dominique Saputro <dominique.saputro@gmail.com>
2025-08-14 11:44:07 +0700

    [FIX] Certain masa end of date not found

--- xml_pph21.py
+++ xml_pph21.py
@@ -148,7 +148,8 @@ if st.button('Run'):
             cert = 'DTP'
         else:
             cert = 'N/A'
-        eod = datetime.datetime(tahun, masa, calendar.monthrange(tahun, masa)[1])
+        last_day = calendar.monthrange(tahun, masa)[1]
+        eod = datetime.datetime(tahun, masa, last_day)
         eod = eod.strftime('%Y-%m-%d')
         filename_xml = bupot_value + '_' + datetime.datetime(tahun,masa,15).strftime('%Y%m')+ '_' + str(npwp) + '.xml'
         if gross:
